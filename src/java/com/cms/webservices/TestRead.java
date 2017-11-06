f8a32c223e0a2f20389a6d69de9f5289f8c54fb4
amit-walia <32356418+amit-walia@users.noreply.github.com>
2017-11-06 14:08:02 +0530

    Update TestRead.java

--- src/java/com/cms/webservices/TestRead.java
+++ src/java/com/cms/webservices/TestRead.java
@@ -48,16 +48,16 @@ public class TestRead
         Identifier toRead = new Identifier();
         Path path = new Path();
         path.setPath("/");
-        path.setSiteName("example.com");
+        path.setSiteName("http://www.groupkt.com/post/5926d648/free-restful-web-service-to-get-location-by-ip-address.htm");
         toRead.setPath(path);
         toRead.setType(EntityTypeString.folder);
 
         Read read = new Read();
         read.setIdentifier(toRead);
 
-        Authentication authentication = new Authentication();
-        authentication.setUsername("admin");
-        authentication.setPassword("admin");
+       // Authentication authentication = new Authentication();
+        //authentication.setUsername("admin");
+        //authentication.setPassword("admin");
 
         AssetOperationHandlerServiceLocator serviceLocator = new AssetOperationHandlerServiceLocator();
         AssetOperationHandler handler = serviceLocator.getAssetOperationService();
